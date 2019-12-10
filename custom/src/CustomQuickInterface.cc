1a634ba9c60c43afa39283bd7c667b5e3c87566e
James Morritt <james1morritt@gmail.com>
2019-12-10 02:13:14 +0000

    Fixed values not returning to default when preset processing option changed

--- custom/src/CustomQuickInterface.cc
+++ custom/src/CustomQuickInterface.cc
@@ -156,68 +156,68 @@ void CustomQuickInterface::setAdvancedSettings(bool advancedSettings) {
 
 
 void CustomQuickInterface::returnToDefault() {
-        _mapping->pcClassify()->rawValue() = _mapping->pcClassify()->rawDefaultValue();
-        _mapping->smrfScalar()->rawValue() = _mapping->smrfScalar()->rawDefaultValue();
-        _mapping->opensfmDepthmapMinPatchSd()->rawValue() = _mapping->opensfmDepthmapMinPatchSd()->rawDefaultValue();
-        _mapping->smrfWindow()->rawValue() = _mapping->smrfWindow()->rawDefaultValue();
-        _mapping->meshOctreeDepth()->rawValue() =_mapping->meshOctreeDepth()->rawDefaultValue();
-        _mapping->minNumFeatures()->rawValue() =_mapping->minNumFeatures()->rawDefaultValue();
-        _mapping->resizeTo()->rawValue() =_mapping->resizeTo()->rawDefaultValue();
-        _mapping->smrfSlope()->rawValue() =_mapping->smrfSlope()->rawDefaultValue();
-        _mapping->rerunFrom()->rawValue() =_mapping->rerunFrom()->rawDefaultValue();
-        _mapping->use3dmesh()->rawValue() =_mapping->use3dmesh()->rawDefaultValue();
-        _mapping->orthophotoCompression()->rawValue() =_mapping->orthophotoCompression()->rawDefaultValue();
-        _mapping->mveConfidence()->rawValue() = _mapping->mveConfidence()->rawDefaultValue();
-        _mapping->texturingSkipHoleFilling()->rawValue() = _mapping->texturingSkipHoleFilling()->rawDefaultValue();
-        _mapping->texturingSkipGlobalSeamLeveling()->rawValue() = _mapping->texturingSkipGlobalSeamLeveling()->rawDefaultValue();
-        _mapping->texturingNadirWeight()->rawValue() = _mapping->texturingNadirWeight()->rawDefaultValue();
-        _mapping->texturingOutlierRemovalType()->rawValue() = _mapping->texturingOutlierRemovalType()->rawDefaultValue();
-        _mapping->othrophotoResolution()->rawValue() = _mapping->othrophotoResolution()->rawDefaultValue();
-        _mapping->dtm()->rawValue() = _mapping->dtm()->rawDefaultValue();
-        _mapping->orthophotoNoTiled()->rawValue() = _mapping->orthophotoNoTiled()->rawDefaultValue();
-        _mapping->demResolution()->rawValue() = _mapping->demResolution()->rawDefaultValue();
-        _mapping->meshSize()->rawValue() = _mapping->meshSize()->rawDefaultValue();
-        _mapping->forceGPS()->rawValue() = _mapping->forceGPS()->rawDefaultValue();
-        _mapping->ignoreGsd()->rawValue() = _mapping->ignoreGsd()->rawDefaultValue();
-        _mapping->buildOverviews()->rawValue() = _mapping->buildOverviews()->rawDefaultValue();
-        _mapping->opensfmDense()->rawValue() = _mapping->opensfmDense()->rawDefaultValue();
-        _mapping->opensfmDepthmapMinConsistentViews()->rawValue() = _mapping->opensfmDepthmapMinConsistentViews()->rawDefaultValue();
-        _mapping->texturingSkipLocalSeamLeveling()->rawValue() = _mapping->texturingSkipLocalSeamLeveling()->rawDefaultValue();
-        _mapping->texturingKeepUnseenFaces()->rawValue() = _mapping->texturingKeepUnseenFaces()->rawDefaultValue();
-        _mapping->debug()->rawValue() = _mapping->debug()->rawDefaultValue();
-        _mapping->useExif()->rawValue() = _mapping->useExif()->rawDefaultValue();
-        _mapping->meshSamples()->rawValue() = _mapping->meshSamples()->rawDefaultValue();
-        _mapping->pcSample()->rawValue() = _mapping->pcSample()->rawDefaultValue();
-        _mapping->matcherDistance()->rawValue() = _mapping->matcherDistance()->rawDefaultValue();
-        _mapping->splitOverlap()->rawValue() = _mapping->splitOverlap()->rawDefaultValue();
-        _mapping->demDecimation()->rawValue() = _mapping->demDecimation()->rawDefaultValue();
-        _mapping->orthophotoCutline()->rawValue() = _mapping->orthophotoCutline()->rawDefaultValue();
-        _mapping->pcFilter()->rawValue() = _mapping->pcFilter()->rawDefaultValue();
-        _mapping->split()->rawValue() = _mapping->split()->rawDefaultValue();
-        _mapping->fastOrthophoto()->rawValue() = _mapping->fastOrthophoto()->rawDefaultValue();
-        _mapping->pcEpt()->rawValue() = _mapping->pcEpt()->rawDefaultValue();
-        _mapping->crop()->rawValue() = _mapping->crop()->rawDefaultValue();
-        _mapping->pcLas()->rawValue() = _mapping->pcLas()->rawDefaultValue();
-        _mapping->merge()->rawValue() = _mapping->merge()->rawDefaultValue();
-        _mapping->dsm()->rawValue() = _mapping->dsm()->rawDefaultValue();
-        _mapping->demGapfillSteps()->rawValue() = _mapping->demGapfillSteps()->rawDefaultValue();
-        _mapping->meshPointWeight()->rawValue() = _mapping->meshPointWeight()->rawDefaultValue();
-        _mapping->maxConcurrency()->rawValue() = _mapping->maxConcurrency()->rawDefaultValue();
-        _mapping->texturingToneMapping()->rawValue() = _mapping->texturingToneMapping()->rawDefaultValue();
-        _mapping->demEuclidianMap()->rawValue() = _mapping->demEuclidianMap()->rawDefaultValue();
-        _mapping->cameraLens()->rawValue() = _mapping->cameraLens()->rawDefaultValue();
-        _mapping->skip3dmodel()->rawValue() = _mapping->skip3dmodel()->rawDefaultValue();
-        _mapping->matchNeighbours()->rawValue() = _mapping->matchNeighbours()->rawDefaultValue();
-        _mapping->pcCsv()->rawValue() = _mapping->pcCsv()->rawDefaultValue();
-        _mapping->endWith()->rawValue() = _mapping->endWith()->rawDefaultValue();
-        _mapping->depthmapResolution()->rawValue() = _mapping->depthmapResolution()->rawDefaultValue();
-        _mapping->texturingDataTerm()->rawValue() = _mapping->texturingDataTerm()->rawDefaultValue();
-        _mapping->texturingSkipVisibilityTest()->rawValue() = _mapping->texturingSkipVisibilityTest()->rawDefaultValue();
-        _mapping->opensfmDepthmapMethod()->rawValue() = _mapping->opensfmDepthmapMethod()->rawDefaultValue();
-        _mapping->useFixedCameraParams()->rawValue() = _mapping->useFixedCameraParams()->rawDefaultValue();
-        _mapping->smrfThreshold()->rawValue() = _mapping->smrfThreshold()->rawDefaultValue();
-        _mapping->verbose()->rawValue() = _mapping->verbose()->rawDefaultValue();
-        _mapping->useHybridBundleAdjustment()->rawValue() = _mapping->useHybridBundleAdjustment()->rawDefaultValue();
+        _mapping->pcClassify()->setRawValue( _mapping->pcClassify()->rawDefaultValue());
+        _mapping->smrfScalar()->setRawValue( _mapping->smrfScalar()->rawDefaultValue());
+        _mapping->opensfmDepthmapMinPatchSd()->setRawValue( _mapping->opensfmDepthmapMinPatchSd()->rawDefaultValue());
+        _mapping->smrfWindow()->setRawValue( _mapping->smrfWindow()->rawDefaultValue());
+        _mapping->meshOctreeDepth()->setRawValue( _mapping->meshOctreeDepth()->rawDefaultValue());
+        _mapping->minNumFeatures()->setRawValue( _mapping->minNumFeatures()->rawDefaultValue());
+        _mapping->resizeTo()->setRawValue( _mapping->resizeTo()->rawDefaultValue());
+        _mapping->smrfSlope()->setRawValue( _mapping->smrfSlope()->rawDefaultValue());
+        _mapping->rerunFrom()->setRawValue( _mapping->rerunFrom()->rawDefaultValue());
+        _mapping->use3dmesh()->setRawValue( _mapping->use3dmesh()->rawDefaultValue());
+        _mapping->orthophotoCompression()->setRawValue( _mapping->orthophotoCompression()->rawDefaultValue());
+        _mapping->mveConfidence()->setRawValue( _mapping->mveConfidence()->rawDefaultValue());
+        _mapping->texturingSkipHoleFilling()->setRawValue( _mapping->texturingSkipHoleFilling()->rawDefaultValue());
+        _mapping->texturingSkipGlobalSeamLeveling()->setRawValue( _mapping->texturingSkipGlobalSeamLeveling()->rawDefaultValue());
+        _mapping->texturingNadirWeight()->setRawValue( _mapping->texturingNadirWeight()->rawDefaultValue());
+        _mapping->texturingOutlierRemovalType()->setRawValue( _mapping->texturingOutlierRemovalType()->rawDefaultValue());
+        _mapping->othrophotoResolution()->setRawValue( _mapping->othrophotoResolution()->rawDefaultValue());
+        _mapping->dtm()->setRawValue( _mapping->dtm()->rawDefaultValue());
+        _mapping->orthophotoNoTiled()->setRawValue( _mapping->orthophotoNoTiled()->rawDefaultValue());
+        _mapping->demResolution()->setRawValue( _mapping->demResolution()->rawDefaultValue());
+        _mapping->meshSize()->setRawValue( _mapping->meshSize()->rawDefaultValue());
+        _mapping->forceGPS()->setRawValue( _mapping->forceGPS()->rawDefaultValue());
+        _mapping->ignoreGsd()->setRawValue( _mapping->ignoreGsd()->rawDefaultValue());
+        _mapping->buildOverviews()->setRawValue( _mapping->buildOverviews()->rawDefaultValue());
+        _mapping->opensfmDense()->setRawValue( _mapping->opensfmDense()->rawDefaultValue());
+        _mapping->opensfmDepthmapMinConsistentViews()->setRawValue( _mapping->opensfmDepthmapMinConsistentViews()->rawDefaultValue());
+        _mapping->texturingSkipLocalSeamLeveling()->setRawValue( _mapping->texturingSkipLocalSeamLeveling()->rawDefaultValue());
+        _mapping->texturingKeepUnseenFaces()->setRawValue( _mapping->texturingKeepUnseenFaces()->rawDefaultValue());
+        _mapping->debug()->setRawValue( _mapping->debug()->rawDefaultValue());
+        _mapping->useExif()->setRawValue( _mapping->useExif()->rawDefaultValue());
+        _mapping->meshSamples()->setRawValue( _mapping->meshSamples()->rawDefaultValue());
+        _mapping->pcSample()->setRawValue( _mapping->pcSample()->rawDefaultValue());
+        _mapping->matcherDistance()->setRawValue( _mapping->matcherDistance()->rawDefaultValue());
+        _mapping->splitOverlap()->setRawValue( _mapping->splitOverlap()->rawDefaultValue());
+        _mapping->demDecimation()->setRawValue( _mapping->demDecimation()->rawDefaultValue());
+        _mapping->orthophotoCutline()->setRawValue( _mapping->orthophotoCutline()->rawDefaultValue());
+        _mapping->pcFilter()->setRawValue( _mapping->pcFilter()->rawDefaultValue());
+        _mapping->split()->setRawValue( _mapping->split()->rawDefaultValue());
+        _mapping->fastOrthophoto()->setRawValue( _mapping->fastOrthophoto()->rawDefaultValue());
+        _mapping->pcEpt()->setRawValue( _mapping->pcEpt()->rawDefaultValue());
+        _mapping->crop()->setRawValue( _mapping->crop()->rawDefaultValue());
+        _mapping->pcLas()->setRawValue( _mapping->pcLas()->rawDefaultValue());
+        _mapping->merge()->setRawValue( _mapping->merge()->rawDefaultValue());
+        _mapping->dsm()->setRawValue( _mapping->dsm()->rawDefaultValue());
+        _mapping->demGapfillSteps()->setRawValue( _mapping->demGapfillSteps()->rawDefaultValue());
+        _mapping->meshPointWeight()->setRawValue( _mapping->meshPointWeight()->rawDefaultValue());
+        _mapping->maxConcurrency()->setRawValue( _mapping->maxConcurrency()->rawDefaultValue());
+        _mapping->texturingToneMapping()->setRawValue( _mapping->texturingToneMapping()->rawDefaultValue());
+        _mapping->demEuclidianMap()->setRawValue( _mapping->demEuclidianMap()->rawDefaultValue());
+        _mapping->cameraLens()->setRawValue( _mapping->cameraLens()->rawDefaultValue());
+        _mapping->skip3dmodel()->setRawValue( _mapping->skip3dmodel()->rawDefaultValue());
+        _mapping->matchNeighbours()->setRawValue( _mapping->matchNeighbours()->rawDefaultValue());
+        _mapping->pcCsv()->setRawValue( _mapping->pcCsv()->rawDefaultValue());
+        _mapping->endWith()->setRawValue( _mapping->endWith()->rawDefaultValue());
+        _mapping->depthmapResolution()->setRawValue( _mapping->depthmapResolution()->rawDefaultValue());
+        _mapping->texturingDataTerm()->setRawValue( _mapping->texturingDataTerm()->rawDefaultValue());
+        _mapping->texturingSkipVisibilityTest()->setRawValue( _mapping->texturingSkipVisibilityTest()->rawDefaultValue());
+        _mapping->opensfmDepthmapMethod()->setRawValue( _mapping->opensfmDepthmapMethod()->rawDefaultValue());
+        _mapping->useFixedCameraParams()->setRawValue( _mapping->useFixedCameraParams()->rawDefaultValue());
+        _mapping->smrfThreshold()->setRawValue( _mapping->smrfThreshold()->rawDefaultValue());
+        _mapping->verbose()->setRawValue( _mapping->verbose()->rawDefaultValue());
+        _mapping->useHybridBundleAdjustment()->setRawValue( _mapping->useHybridBundleAdjustment()->rawDefaultValue());
 }
 
 void CustomQuickInterface::dsmdtm() {
@@ -269,29 +269,26 @@ void CustomQuickInterface::forest() {
 }
 
 void CustomQuickInterface::presetChanged() {
-    QGCToolbox* toolbox = qgcApp()->toolbox();
-    // Be careful of toolbox not being open yet
-    if (toolbox) {
-        returnToDefault();
-
-        std::string value = _mapping->processingPresets()->enumStringValue().toStdString();
-        if (value == "DSM + DTM") {
-                dsmdtm();
-        } else if (value == "High Resolution") {
-                highRes();
-        } else if (value == "Fast Orthophoto") {
-                fastOrtho();
-        } else if (value == "Forest") {
-                forest();
-        } else if (value == "Point of Interest") {
-                pointOfInterest();
-        } else if (value == "Buildings") {
-                buildings();
-        } else if (value == "3D Model") {
-                model();
-        } else if (value == "Volume Analysis") {
-                volumeAnalysis();
-        }
+    
+    returnToDefault();
+    
+    std::string value = _mapping->processingPresets()->enumStringValue().toStdString();
+    if (value == "DSM + DTM") {
+        dsmdtm();
+    } else if (value == "High Resolution") {
+        highRes();
+    } else if (value == "Fast Orthophoto") {
+        fastOrtho();
+    } else if (value == "Forest") {
+        forest();
+    } else if (value == "Point of Interest") {
+        pointOfInterest();
+    } else if (value == "Buildings") {
+        buildings();
+    } else if (value == "3D Model") {
+        model();
+    } else if (value == "Volume Analysis") {
+        volumeAnalysis();
     }
 }
 
